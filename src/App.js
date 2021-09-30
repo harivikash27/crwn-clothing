dc973671f186ecdd69ade0362169cccf4637d68c
harivikash27 <harivikash@troopr.io>
2021-09-30 19:17:54 +0530

    added new file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch,Route } from 'react-router';
 
+const Hats=()=>(
+  <div>
+    <h1>Hats Pages</h1>
+  </div>
+)
 function App() {
   return (
-    <HomePage/>
+    <div>
+    <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route  path="/hats" component={Hats}/>
+    </Switch>
+    </div>
+    
   );
 }
 
